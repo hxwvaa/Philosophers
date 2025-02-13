31d2f5aed1fd78f750a86782f78474368a50040e
hxwvaa <hammdhanb8@gmail.com>
2025-02-13 17:41:31 +0400

    actual final insha'Allah

--- philo/philo_monitor.c
+++ philo/philo_monitor.c
@@ -6,7 +6,7 @@
 /*   By: hbasheer <hbasheer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 18:41:26 by hbasheer          #+#    #+#             */
-/*   Updated: 2025/02/10 11:24:22 by hbasheer         ###   ########.fr       */
+/*   Updated: 2025/02/13 17:38:03 by hbasheer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ void	*monitor_routine(void *arg)
 				meals_concluded(data);
 				return (NULL);
 			}
-			if ((__int128_t) get_time() - (__int128_t)data->philos[i].last_eat > data->time_to_die)
+			if (get_time() > data->philos[i].last_eat + data->time_to_die)
 			{
 				philo_died(data, i);
 				return (NULL);
